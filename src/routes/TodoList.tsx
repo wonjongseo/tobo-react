d902079e7977f6f415d75b5ec750fe10d9ca14f3
wonjongseo <visionwill@naver.com>
2022-05-29 12:45:28 +0900

    react-hook-form

--- src/routes/TodoList.tsx
+++ src/routes/TodoList.tsx
@@ -1,27 +1,105 @@
-import {FormEvent, useState} from "react";
+import {useForm} from "react-hook-form";
+
+interface IForm {
+    email: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    password2: string;
+    extraError?: string;
+}
 
 const TodoList = () => {
-    const [todo, setTodo] = useState("");
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+        setError,
+    } = useForm<IForm>({
+        defaultValues: {
+            email: "@naver.com",
+        },
+    });
 
-    const onChange = (event: FormEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: {value},
-        } = event;
-        setTodo(value);
+    const onValid = (data: IForm) => {
+        if (data.password !== data.password2) {
+            setError(
+                "password2",
+                {message: "Password are not the same"},
+                {shouldFocus: true}
+            );
+        }
+        // setError("extraError", {message: "Server Offline"});
     };
 
-    const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(todo);
-    };
     return (
         <div>
-            <form onSubmit={onSubmit}>
+            <form
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                }}
+                onSubmit={handleSubmit(onValid)}
+            >
                 <input
-                    onChange={onChange}
-                    value={todo}
-                    placeholder="White a to do"
+                    {...register("email", {
+                        required: "Email is required",
+                        pattern: {
+                            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+                            message: "Only naver.com emails allowed",
+                        },
+                    })}
+                    placeholder="email"
                 />
+                <span>{errors?.email?.message}</span>
+                <input
+                    {...register("firstName", {
+                        required: "Write a something",
+                        validate: {
+                            oneCheck: (value) =>
+                                value.includes("one") ? "No one allowds" : true,
+                            thowCheck: (value) =>
+                                value.includes("two") ? "No two allowds" : true,
+                        },
+                    })}
+                    placeholder="first name"
+                />
+                <span>{errors?.firstName?.message}</span>
+                <input
+                    {...register("lastName", {
+                        required: "Write a something",
+                    })}
+                    placeholder="last name"
+                />
+                <span>{errors?.lastName?.message}</span>
+                <input
+                    {...register("username", {
+                        required: "Write a something",
+                        minLength: {
+                            value: 5,
+                            message: "Too Short",
+                        },
+                    })}
+                    placeholder="username"
+                />
+                <span>{errors?.username?.message}</span>
+                <input
+                    {...register("password", {
+                        required: true,
+                        minLength: 5,
+                    })}
+                    placeholder="password"
+                />
+                <span>{errors?.password?.message}</span>
+                <input
+                    {...register("password2", {
+                        required: "Write a something",
+                        minLength: 5,
+                    })}
+                    placeholder="password2"
+                />
+                <span>{errors?.password2?.message}</span>
                 <button>Add</button>
             </form>
         </div>
